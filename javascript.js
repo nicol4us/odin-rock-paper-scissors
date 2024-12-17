a16c80dd9aad246f077ace7d4e87f751ce270e4a
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2024-12-17 11:56:37 +0700

    Add playGame function in to javascript.js file

--- javascript.js
+++ javascript.js
@@ -40,49 +40,71 @@ function getHumanChoice() {
     }
 }
 
-// (String, String) -> ()
-// To adjust computerScore and humanScore variable and print message of the winnger
-function playRound(humanChoice, computerChoice) {
-    switch(true) {
-        case humanChoice === "rock" && computerChoice === "rock": 
-            console.log("You are draw against computer");
-            break;
-        case humanChoice === "rock" && computerChoice === "paper":
-            console.log("You loose, paper win again rock");
-            computerScore = computerScore + 1;
-            break;
-        case humanChoice === "rock" && computerChoice === "scissors":
-            console.log("Congratulations, rock wins agains scissors");
-            humanScore = humanScore + 1 ;
-            break;
-        case humanChoice === "paper" && computerChoice === "rock":
-            console.log("Congratulations, paper wins agains rock");
-            humanScore = humanScore + 1;
-            break;
-        case humanChoice === "paper" && computerChoice === "paper":
-            console.log("You are draw against computer");            
-            break;
-        case humanChoice === "paper" && computerChoice === "scissor":
-            console.log("You loose, scissor wins against paper");
-            computerScore = computerScore +1;
-            break;
-        case humanChoice === "scissors" && computerChoice === "rock":
-            console.log("You loose, rock wins agains scissors");
-            computerScore = computerScore + 1;
-            break;
-        case humanChoice === "scissors" && computerChoice === "paper":
-            console.log("Congratulations, scissors wins agains paper");
-            humanScore = humanScore + 1;
-            break;
-        case humanChoice === "scissors" && computerChoice === "scissors":
-            console.log("You are draw against computer");
-            break;
-        case humanChoice === null:
-            console.log("You loose, you type wrong option");
-            computerScore =  computerScore + 1;
-            break
+
+
+// () -> ()
+// To run playAround function for 5 times and declare the winner between human and computer
+function playGame() {
+    let humanScore      = 0;
+    let computerScore   = 0;
+    let initialPlay     = 1;
+    let drawScore       = 0;
+    const maxPlay       = 5;
+
+    // (String, String) -> ()
+    // To adjust computerScore and humanScore variable and print message of the winnger
+    function playRound(humanChoice, computerChoice) {
+        switch(true) {
+            case humanChoice === "rock" && computerChoice === "rock": 
+                console.log("You are draw against computer");
+                drawScore = drawScore +1;
+                break;
+            case humanChoice === "rock" && computerChoice === "paper":
+                console.log("You loose, paper win again rock");
+                computerScore = computerScore + 1;
+                break;
+            case humanChoice === "rock" && computerChoice === "scissors":
+                console.log("Congratulations, rock wins agains scissors");
+                humanScore = humanScore + 1 ;
+                break;
+            case humanChoice === "paper" && computerChoice === "rock":
+                console.log("Congratulations, paper wins agains rock");
+                humanScore = humanScore + 1;
+                break;
+            case humanChoice === "paper" && computerChoice === "paper":
+                console.log("You are draw against computer");
+                drawScore = drawScore + 1;            
+                break;
+            case humanChoice === "paper" && computerChoice === "scissor":
+                console.log("You loose, scissor wins against paper");
+                computerScore = computerScore +1;
+                break;
+            case humanChoice === "scissors" && computerChoice === "rock":
+                console.log("You loose, rock wins agains scissors");
+                computerScore = computerScore + 1;
+                break;
+            case humanChoice === "scissors" && computerChoice === "paper":
+                console.log("Congratulations, scissors wins agains paper");
+                humanScore = humanScore + 1;
+                break;
+            case humanChoice === "scissors" && computerChoice === "scissors":
+                console.log("You are draw against computer");
+                drawScore = drawScore + 1;
+                break;
+            case humanChoice === null:
+                console.log("You loose, you type wrong option");
+                computerScore =  computerScore + 1;
+                break;
+    }
+    }
+    for (initialPlay; initialPlay <= maxPlay; initialPlay++) {
+        playRound(getHumanChoice(), getComputerChoice());   
+        
+        console.log(`You have played ${initialPlay} of ${maxPlay}`)
     }
 }
+
+playGame();
 
 
 
